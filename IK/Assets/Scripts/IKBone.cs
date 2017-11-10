b4b133fbf71680914f66e59affa0e1cf972143fb
Unknown <henrybell96@hotmail.com>
2017-11-10 00:20:45 +0000

    Created IK Bone Class

---
+++ IK/Assets/Scripts/IKBone.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKBone {
+
+    public Vector3 pStartNode { get { return mStartNode; } }
+    public Vector3 pEndNode { get { return mEndNode; } }
+    public Vector3 pBoneVector { get { return mEndNode - mStartNode; } }
+
+    private Vector3 mStartNode = Vector3.zero;
+    private Vector3 mEndNode = Vector3.zero;
+    private float mLength = 0.0f;
+
+
+    //-----------------------------------Public Functions----------------------------------
+
+    public IKBone(float length)
+    {
+        mLength = length;
+    }
+
+    public void PositionStartNode(Vector3 pos)
+    {
+        PositionNode(ref mStartNode, pos, ref mEndNode, -1.0f);
+    }
+
+    public void PositionEndNode(Vector3 pos)
+    {
+        PositionNode(ref mEndNode, pos, ref mStartNode, 1.0f);
+    }
+
+    public void Render()
+    {
+        MonoBehaviour.FindObjectOfType<PostRender>().DrawLine(mStartNode, mEndNode);
+    }
+
+    public void RenderGizmosBasic(Color col)
+    {
+        Gizmos.color = col;
+        Gizmos.DrawLine(mStartNode, mEndNode);
+    }
+
+    public void RenderGizmos(Color col)
+    {
+        RenderGizmosBasic(col);
+        Gizmos.color = col;
+        Gizmos.DrawWireSphere(pStartNode, 0.1f);
+        Gizmos.DrawWireSphere(pEndNode, 0.05f);
+    }
+
+
+    //-----------------------------------Private Functions----------------------------------
+
+    private void PositionNode(ref Vector3 node, Vector3 pos, ref Vector3 offsetNode, float offsetMultiplier)
+    {
+        node = pos;
+        offsetNode += pBoneVector.normalized * (pBoneVector.magnitude - mLength) * offsetMultiplier;
+    }
+}
--- IK/Assets/Scripts/IKRoot.cs
+++ IK/Assets/Scripts/IKRoot.cs
@@ -2,99 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IKNode
-{
-    public Vector3 Pos;
-    public float Length;
-
-    public Vector3 pParentPos { get { return mParent == null ? mParentTrans.position : mParent.Pos; } }
-    public IKNode pParent { set { mParent = value; } }
-    public Transform pParentTrans { set { mParentTrans = value; } }
-
-    private IKNode mParent;
-    private Transform mParentTrans;
-}
-
-public class IKRoot : MonoBehaviour
-{
+public class IKRoot : MonoBehaviour {
 
     [SerializeField]
     private Transform TargetTrans;
     [SerializeField]
-    private float NodeTranslationSpeed = 10.0f;
-    [SerializeField]
     private int NodeCount = 5;
     [SerializeField]
-    private float ArmLength = 1.0f;
+    private float Length = 1.0f;
+    [SerializeField]
+    private bool AdvancedGizmos = false;
 
-    private List<IKNode> mNodes = new List<IKNode>();
-    private PostRender mPostRender;
+    private List<IKBone> mBones = new List<IKBone>();
 
-    private IKNode pEffector { get { return mNodes[mNodes.Count - 1]; } }
+    private IKBone pEffector { get { return mBones[mBones.Count - 1]; } }
 
 
     //-----------------------------------Unity Functions-----------------------------------
 
-    private void Awake()
-    {
-        mPostRender = FindObjectOfType<PostRender>();
-    }
-
     private void Start()
     {
+        float boneLength = Length / NodeCount;
         for (int i = 0; i < NodeCount; i++)
-        {
-            mNodes.Add(new IKNode());
-            mNodes[i].Pos = Vector3.one;
-            mNodes[i].Length = ArmLength / NodeCount;
-            if (i == 0) mNodes[i].pParentTrans = this.transform;
-            else mNodes[i].pParent = mNodes[i - 1];
-        }
+            mBones.Add(new IKBone(boneLength));
     }
 
     private void Update()
     {
         Vector3 targetPos = TargetTrans.position;
 
-        // Looping from the effector node to the root node.
-        for (int i = mNodes.Count - 1; i >= 0; i--)
+        for (int i = mBones.Count - 1; i >= 0; i--)
         {
-            // Translating the node to its target pos.
-            var translation = targetPos - mNodes[i].Pos;
-            var translationMultiplier = Mathf.Clamp(Time.deltaTime * NodeTranslationSpeed, 0.0f, translation.magnitude);
-            mNodes[i].Pos += translation.normalized * translationMultiplier;
-
-            // Calculating point on the vector that is the bone length from the current node in
-            // the direction of its parent and setting this to be the parents target pos.
-            Vector3 nodeToParent = (mNodes[i].Pos - mNodes[i].pParentPos).normalized;
-            targetPos = mNodes[i].Pos - nodeToParent * mNodes[i].Length;
+            mBones[i].PositionEndNode(targetPos);
+            targetPos = mBones[i].pStartNode;
         }
 
-        // Looping from the root node to the effector node.
-        for (int i = 0; i < mNodes.Count; i++)
+        for (int i = 0; i < mBones.Count; i++)
         {
-            // Moving the node back towards their parent so that they 
-            // are no more than the maximum distance away from them.
-            Vector3 parentToNode = (mNodes[i].Pos - mNodes[i].pParentPos).normalized;
-            mNodes[i].Pos = mNodes[i].pParentPos + parentToNode * mNodes[i].Length;
+            Vector3 parentEndPos = (i == 0) ? this.transform.position : mBones[i-1].pEndNode;
+            mBones[i].PositionStartNode(parentEndPos);
         }
 
-        // Rendering the IK limb.
-        foreach (var node in mNodes)
-            mPostRender.DrawLine(node.Pos, node.pParentPos);
+        foreach (var bone in mBones)
+            bone.Render();
     }
 
     private void OnDrawGizmos()
     {
-        if (!Application.isPlaying) return;
-
-        Gizmos.color = Color.magenta;
-        Gizmos.DrawSphere(this.transform.position, 0.2f);
-        for (int i = 0; i < mNodes.Count; i++)
+        for (int i = 0; i < mBones.Count; i++)
         {
-            Gizmos.DrawSphere(mNodes[i].Pos, 0.1f);
-            Vector3 lineDirection = (mNodes[i].Pos - mNodes[i].pParentPos).normalized;
-            Gizmos.DrawLine(mNodes[i].Pos, mNodes[i].Pos - lineDirection * mNodes[i].Length);
+            Color boneCol = (i % 2 == 0) ? Color.red : Color.magenta;
+            if (AdvancedGizmos) mBones[i].RenderGizmosBasic(boneCol);
+            else mBones[i].RenderGizmos(boneCol);
         }
     }
 }
